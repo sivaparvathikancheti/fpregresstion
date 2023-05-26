41a7f3a40592d59c22d29d5beda6b86cacbc605e
sivaparvathikancheti <sivaparvathikancheti8@gmail.com>
2023-05-26 15:07:02 +0530

    addchanges

--- src/com/sivaram/automation/DemoJob.java
+++ src/com/sivaram/automation/DemoJob.java
@@ -1,9 +1,9 @@
 package com.sivaram.automation;
 
-public class PractDemo7 {
+public class DemoJob {
 
 	public static void main(String[] args) {
-	System.out.println("ram");	
+		// TODO Auto-generated method stub
 
 	}
 
